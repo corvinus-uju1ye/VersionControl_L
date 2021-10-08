62433a970237a2cb98e9971d63137f7d79bfe778
Laura Éva Ónodi <laura.onodi@stud.uni-corvinus.hu>
2021-10-08 22:40:03 +0200

    Add worksheet

--- ExcelExportWeek4/ExcelExportWeek4/Form1.cs
+++ ExcelExportWeek4/ExcelExportWeek4/Form1.cs
@@ -16,17 +16,52 @@ namespace ExcelExportWeek4
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
-
+        Excel.Application xlApp;
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet;
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         public void LoadData ()
         {
             lakasok = context.Flats.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                // CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+
+            }
+            
+        }
+
+        private void CreateTable()
+        { 
+         
         }
     }
 }
