dc644bfe34a726fdd748b7b142bc08218d760267
Laura Éva Ónodi <laura.onodi@stud.uni-corvinus.hu>
2021-11-12 19:29:06 +0100

    create conveyor

--- week08ToyFactory/week08ToyFactory/Form1.cs
+++ week08ToyFactory/week08ToyFactory/Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08ToyFactory.Entities;
 
 namespace week08ToyFactory
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        private BallFactory _ballFactory;
+
+        public BallFactory BallFactory
+        {
+            get { return _ballFactory; }
+            set { _ballFactory = value; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            BallFactory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = BallFactory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
