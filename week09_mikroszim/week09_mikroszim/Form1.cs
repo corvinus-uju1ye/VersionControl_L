a349ec726b75c412df247e30defd581c901eb4de
Laura Éva Ónodi <laura.onodi@stud.uni-corvinus.hu>
2021-12-03 22:35:19 +0100

    szimuláció

--- week09_mikroszim/week09_mikroszim/Form1.cs
+++ week09_mikroszim/week09_mikroszim/Form1.cs
@@ -27,7 +27,23 @@ namespace week09_mikroszim
             BirthProbabilities = GetBirthProbabilities(@"C:\Windows\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Windows\Temp\halál.csv");
 
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
 
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvPath)
